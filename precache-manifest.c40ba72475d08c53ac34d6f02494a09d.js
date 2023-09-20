ed9f272e2771df802aa57f247d1ce8fb810c288c
tomasbrezina <tomas.brezinaaa@gmail.com>
2023-09-20 21:46:27 +0200

    Updates

---
+++ precache-manifest.c40ba72475d08c53ac34d6f02494a09d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d9a48bb0b2e7c9d6e28",
+    "url": "/bmi-app/static/css/main.cf922733.chunk.css"
+  },
+  {
+    "revision": "2d9a48bb0b2e7c9d6e28",
+    "url": "/bmi-app/static/js/main.2d9a48bb.chunk.js"
+  },
+  {
+    "revision": "947184f4cfdb53f0cabe",
+    "url": "/bmi-app/static/js/1.947184f4.chunk.js"
+  },
+  {
+    "revision": "1c2b897823113221d7af",
+    "url": "/bmi-app/static/js/runtime~main.1c2b8978.js"
+  },
+  {
+    "revision": "9b1ac4497a2d86f39ad5b1779ca09224",
+    "url": "/bmi-app/index.html"
+  }
+];
